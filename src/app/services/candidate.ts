2b1f13a53461ec591f651ba3d15b1330199389c7
Mauricio Contreras <139881015+mauricio-contreras@users.noreply.github.com>
2025-07-06 17:45:31 +0200

    Add CandidateService and associated tests; implemented candidate management functionality including upload, persistence, and retrieval from local storage.

---
+++ src/app/services/candidate.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+
+export interface Candidate {
+  name: string;
+  surname: string;
+  seniority: 'junior'| 'mid-level' | 'senior';
+  years: number;
+  availability: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CandidateService {
+  private readonly apiUrl = 'http://localhost:3000/candidates';
+  private readonly candidatesKey = 'loadedCandidates';
+
+  private readonly _newCandidate = new BehaviorSubject<Candidate | null>(null);
+  readonly newCandidate$ = this._newCandidate.asObservable();
+
+  constructor(
+    private readonly http: HttpClient,
+  ) { }
+
+  uploadCandidate(formData: FormData): Observable<Candidate> {
+    return this.http.post<Candidate>(this.apiUrl, formData).pipe(
+      tap((candidate: Candidate) => {
+        this._newCandidate.next(candidate);
+      })
+    )
+  }
+
+  getPersistedCandidates(): Candidate[] {
+    const persistedData = localStorage.getItem(this.candidatesKey);
+    return persistedData ? JSON.parse(persistedData) : [];
+  }
+
+  persistCandidates(candidates: Candidate[]): void {
+    localStorage.setItem(this.candidatesKey, JSON.stringify(candidates));
+  }
+
+  clearPersistedCandidates(): void {
+    localStorage.removeItem(this.candidatesKey);
+  }
+}
